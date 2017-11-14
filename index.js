39a4bfb168f280ce933aa7a37131c271146d9c34
paulsbond <github@paulsbond.co.uk>
2017-11-14 11:20:12 +0000

    Set background to documentElement.clientHeight

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 bg = document.getElementById("background");
 
 function resizeBackgroud() {
-  bg.style.height = window.screen.height + "px";
+  bg.style.height = document.documentElement.clientHeight + "px";
 }
 
 window.addEventListener("resize", resizeBackgroud);
