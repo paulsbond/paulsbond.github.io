3f858d60d58bb1fb7a38a879ffedfb83a05c600d
Paul Bond <git@paulsbond.co.uk>
2022-03-15 07:19:58 +0000

    Re-added background hack

---
+++ index.js
@@ -0,0 +1,8 @@
+bg = document.getElementById("background");
+
+function resizeBackgroud() {
+    bg.style.height = document.documentElement.clientHeight + 100 + "px";
+}
+
+window.addEventListener("resize", resizeBackgroud);
+resizeBackgroud();
